77cec6aa4cc7dc0619b77182532f224ee101ff30
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2020-10-09 00:35:17 +0200

    now forcing https

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?PHP
+    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+        $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $location);
+        exit();
+    }
     session_start();
     if (!isset($_SESSION["state"])) {
         $_SESSION["state"] = rand(100, 999);
